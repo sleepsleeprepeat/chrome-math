986830157b0f2239e50abf3b0cc2b6d09952e3e8
SleepSleepRepeat <d3v.jon@gmail.com>
2023-06-30 20:30:16 +0200

    update

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+const SNIPPET_URL =
+    "https://raw.githubusercontent.com/sleepsleeprepeat/chrome-math/master/math_snippets.json";
+
 async function fetchSnippets() {
-    let res = await fetch("/snippets");
+    let res = await fetch(SNIPPET_URL);
+    let snippets = await res.json();
+    return snippets;
+}
+
+function createContextMenu(snippets) {
+    chrome.contextMenus.create({
+        title: "Math-Snippet hinzufügen",
+        id: "math_snippet",
+    });
+
+    snippets.forEach((category) => {
+        chrome.contextMenus.create({
+            title: category.title,
+            parentId: "math_snippet",
+            id: category.id,
+        });
+
+        category.items.forEach((item) => {
+            chrome.contextMenus.create({
+                title: item.title,
+                parentId: category.id,
+                id: item.id,
+                onclick: () => {
+                    // insert snippet at caret position
+                    chrome.tabs.executeScript({
+                        code: `document.execCommand("insertText", false, "${item.snippet}");`,
+                    });
+                },
+            });
+        });
+    });
 }
